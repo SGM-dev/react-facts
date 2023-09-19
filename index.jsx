be13ac4726755219e6dbeb1896d15a457214c87c
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-19 03:36:14 +0000

    export components

--- index.jsx
+++ index.jsx
@@ -1,42 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img src="./react-logo.png" className="nav-logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-    </>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2023 SGM development. All rights reserved.</small>
-    </footer>
-  );
-}
+import Header from "/Header.jsx";
+import MainContent from "/MainContent.jsx";
+import Footer from "/Footer.jsx";
 
 function Page() {
   return (
