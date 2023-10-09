c5881a80d57cc60ca79a3dadd7a2ee099ee71dda
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-09 05:33:53 +0000

    add toggle for light/dark mode

--- App.jsx
+++ App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import logo from "../images/reactjs-icon.svg";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${props.darkMode ? "dark" : ""}`}>
       <img src={logo} className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
+      <div className='toggle-switch'>
+        <label>
+          <input type='checkbox' onClick={props.toggleDarkMode}></input>
+          <span class='slider'></span>
+        </label>
+      </div>
     </nav>
   );
 }
+
